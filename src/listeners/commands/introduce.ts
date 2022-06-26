6dbb1d8f1417a767782c5f9e10e35b80e6c23aaf
spuggle <spuggle@protonmail.com>
2022-06-27 02:43:47 +0500

    feat(commands): add introduce command

---
+++ src/listeners/commands/introduce.ts
@@ -0,0 +1,21 @@
+import { createSlashCommand, PermissionFlags } from "../../structures/SlashCommand.js";
+import { isNullish } from "../../utilities/nullishAssertion.js";
+
+const introduceCommand = createSlashCommand({
+  name: "introduce",
+  description: "Introduce yourself to this server!",
+  allowInDMs: false,
+  guildPermissions: PermissionFlags.SendMessages,
+  options: [],
+  async run(bot, interaction): Promise<void> {
+    const introductionModal = bot.handlers.modal.listeners.get("introduction")?.modal;
+    if (isNullish(introductionModal) || isNullish(interaction.guild)) return interaction.reply({
+      content: "I could not run this command!",
+      ephemeral: true
+    });
+
+    await interaction.showModal(introductionModal);
+  }
+});
+
+export default introduceCommand;
