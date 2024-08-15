bf87eb4304f4d87cde1ba2d5f4e57018049f4f56
spuggle <spuggle@protonmail.com>
2024-08-15 16:00:30 +0400

    refactor(IntroductionDetails): refactor into own file

---
+++ src/types/IntroductionDetails.ts
@@ -0,0 +1,8 @@
+export interface IntroductionDetails {
+  readonly name: string;
+  readonly iconURL: string;
+  readonly aboutUser: string;
+  readonly userAge: string;
+  readonly userPronouns: string;
+  readonly userHobbies: string;
+}
